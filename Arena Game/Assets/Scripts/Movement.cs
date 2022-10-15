8e09541f6ee5221839f0334ab325b9c2caec308b
UnSpoken12 <JKongShi@gmail.com>
2022-10-14 20:28:52 -0400

    Player scripting movement and enemy sprites and animation

---
+++ Arena Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed = 1.5f;
+    public Animator animator;
+
+    private void Update()
+    {
+        // Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float totalHoriMovement = horizontalInput * Time.deltaTime * speed;
+        float totalVertMovement = verticalInput * Time.deltaTime * speed;
+        transform.Translate(new Vector2(totalHoriMovement, totalVertMovement));
+
+        // Transition to walking animation
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
+        // Change the direction it's facing
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+}
