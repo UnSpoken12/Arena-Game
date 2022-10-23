2973f40347974ce4b192746cbbdc6ee3b162b239
haydensatwit <79537900+raravez@users.noreply.github.com>
2022-10-22 23:53:22 -0400

    Added slime movement

--- Arena Game/Assets/Scripts/Slime.cs
+++ Arena Game/Assets/Scripts/Slime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class Slime : MonoBehaviour
 {
     public int maxHealth = 3;
+    private float speed = .5f;
     int currentHealth;
 
     // Start is called before the first frame update
@@ -16,7 +18,38 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Input
+        Vector2 playerPos = GameObject.Find("Player").transform.position;
+        Vector2 currentPos = this.transform.position;
+        Vector2 displacement = new Vector2((playerPos.x - currentPos.x) * Time.deltaTime, (playerPos.y - currentPos.y) * Time.deltaTime);
+        if (Math.Abs(displacement.magnitude) < speed * Time.deltaTime)
+        {
+            double angle = Vector2.Angle(playerPos, currentPos);
+            float nextX = (float)Math.Cos(angle) * speed * Time.deltaTime;
+            float nextY = (float)Math.Sin(angle) * speed * Time.deltaTime;
+            displacement = new Vector2(nextX, nextY);
+        }
+        //Vector2 targetPos = Vector2.MoveTowards(this.transform.position, new Vector2(playerPos.x, playerPos.y), speed);
+        //double angle = Vector2.Angle(new Vector2(playerPos.x, playerPos.y), this.transform.position);
 
+        // Movement
+        //float totalHoriMovement = targetPos.x * Time.deltaTime;
+        //float totalVertMovement = targetPos.y * Time.deltaTime;
+        //transform.Translate(new Vector2(totalHoriMovement, totalVertMovement));
+        transform.Translate(displacement);
+
+        // Transition to walking animation
+        //animator.SetFloat("Speed", Mathf.Abs(horizontalInput + verticalInput));
+
+        // Change the direction it's facing
+        /*if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }*/
     }
 
     public void Damage()
