97575979c312583c347c2d422e5e6fc16246d8e7
UnSpoken12 <JKongShi@gmail.com>
2022-10-22 23:45:54 -0400

    fixed small bug with enemy death

--- Arena Game/Assets/Scripts/DetectFloor.cs
+++ Arena Game/Assets/Scripts/DetectFloor.cs
@@ -16,9 +16,13 @@ public class DetectFloor : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Void"))
+        if (collision.CompareTag("Void") && gameObject.CompareTag("Player"))
         {
             onFloor = false;
+        }
+        else if(collision.CompareTag("Void"))
+        {
+            Destroy(gameObject);
         }
     }
 }
